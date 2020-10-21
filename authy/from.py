33f6deb0b19645b32a546a44179e731b25ae4865
Ronny <mwithiamoki@gmail.com>
2020-10-22 02:06:12 +0300

    define clean

--- authy/from.py
+++ authy/from.py
@@ -38,3 +38,13 @@ class SignupForm(forms.ModelForm):
 		self.fields['username'].validators.append(InvalidUser)
 		self.fields['username'].validators.append(UniqueUser)
 		self.fields['email'].validators.append(UniqueEmail)
+
+
+    def clean(self):
+		super(SignupForm, self).clean()
+		password = self.cleaned_data.get('password')
+		confirm_password = self.cleaned_data.get('confirm_password')
+
+		if password != confirm_password:
+			self._errors['password'] = self.error_class(['Passwords do not match. Try again'])
+		return self.cleaned_data
