a41660823702288718c65ab19ab379e68f068917
Ronny <mwithiamoki@gmail.com>
2020-10-22 02:01:50 +0300

    created form.py and defined forbiddenusers

---
+++ authy/from.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from authy.models import Profile
+
+def ForbiddenUsers(value):
+	forbidden_users = ['admin', 'css', 'js', 'authenticate', 'login', 'logout', 'administrator', 'root',
+	'email', 'user', 'join', 'sql', 'static', 'python', 'delete']
+	if value.lower() in forbidden_users:
+		raise ValidationError('Invalid name for user, this is a reserverd word.')
