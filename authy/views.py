eebfbfeedb766667cbcf450128c79e627aae2468
Ronny <mwithiamoki@gmail.com>
2020-10-22 01:55:04 +0300

    define signup

--- authy/views.py
+++ authy/views.py
@@ -81,3 +81,20 @@ def UserProfileFavorites(request, username):
 	return HttpResponse(template.render(context, request))
 
 
+def Signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			password = form.cleaned_data.get('password')
+			User.objects.create_user(username=username, email=email, password=password)
+			return redirect('index')
+	else:
+		form = SignupForm()
+	
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'signup.html', context)
