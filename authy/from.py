f3e37383052eba93480da321c51c21009d43b147
Ronny <mwithiamoki@gmail.com>
2020-10-22 02:07:05 +0300

    added class change password form

--- authy/from.py
+++ authy/from.py
@@ -48,3 +48,10 @@ class SignupForm(forms.ModelForm):
 		if password != confirm_password:
 			self._errors['password'] = self.error_class(['Passwords do not match. Try again'])
 		return self.cleaned_data
+
+class ChangePasswordForm(forms.ModelForm):
+	id = forms.CharField(widget=forms.HiddenInput())
+	old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="Old password", required=True)
+	new_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="New password", required=True)
+	confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input is-medium'}), label="Confirm new password", required=True)
+    
