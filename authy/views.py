cd68da7609ae664d0e5b14805645b7461dda6092
Ronny <mwithiamoki@gmail.com>
2020-10-22 01:19:12 +0300

    added follow status

--- authy/views.py
+++ authy/views.py
@@ -30,4 +30,6 @@ def UserProfile(request, username):
     posts_count = Post.objects.filter(user=user).count()
 	following_count = Follow.objects.filter(follower=user).count()
 	followers_count = Follow.objects.filter(following=user).count()
+
+    follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
     
