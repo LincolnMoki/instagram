200fefe25fcb9a67125023064c9b5402811e5e15
Ronny <mwithiamoki@gmail.com>
2020-10-22 01:17:15 +0300

    defined user profile

---
+++ authy/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from authy.forms import SignupForm, ChangePasswordForm, EditProfileForm
+from django.contrib.auth.models import User
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+
+from authy.models import Profile
+from post.models import Post, Follow, Stream
+from django.db import transaction
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from django.core.paginator import Paginator
+
+from django.urls import resolve
+
+def UserProfile(request, username):
+	user = get_object_or_404(User, username=username)
+	profile = Profile.objects.get(user=user)
+	url_name = resolve(request.path).url_name
+	
+	if url_name == 'profile':
+		posts = Post.objects.filter(user=user).order_by('-posted')
+
+	else:
+		posts = profile.favorites.all()
