185913fe20cd057ff59b8f5d24bd97cfa659cbeb
Ronny <mwithiamoki@gmail.com>
2020-10-21 18:41:38 +0300

    added article class andadded img for comments

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.welcome, name='home')
+    path('',views.welcome, name='home'),
+    path('',views.post_details, name='post_details')
 ]    
--- instagram/views.py
+++ instagram/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def welcome(request):
     title = 'welcome'   
     return render(request,'instagram/home.html', {'title': title})
+
+def post_details(request):
+    return render(request,'instagram/post_details.html')
