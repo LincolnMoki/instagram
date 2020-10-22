2e0c31578461bd1733e1b5776a06b8673c356f07
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:21:02 +0300

    defined delete no notification

--- notifications/views.py
+++ notifications/views.py
@@ -17,3 +17,9 @@ def ShowNOtifications(request):
 
 	return HttpResponse(template.render(context, request))
 
+
+def DeleteNotification(request, noti_id):
+	user = request.user
+	Notification.objects.filter(id=noti_id, user=user).delete()
+	return redirect('show-notifications')
+
