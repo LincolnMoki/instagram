1af62401ac811f971c315220234f7b593b629fa5
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:39:30 +0300

    updates

--- comment/models.py
+++ comment/models.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save, post_delete
 
 
 class Comment(models.Model):
-	# post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = models.TextField()
 	date = models.DateTimeField(auto_now_add=True)
@@ -28,5 +28,5 @@ def user_del_comment_post(sender, instance, *args, **kwargs):
     notify = Notification.objects.filter(post=post, sender=sender, notification_type=2)
     notify.delete()    
 
-# post_save.connect(Comment.user_comment_post, sender=Comment)
-# post_delete.connect(Comment.user_del_comment_post, sender=Comment)
+ post_save.connect(Comment.user_comment_post, sender=Comment)
+ post_delete.connect(Comment.user_del_comment_post, sender=Comment)
