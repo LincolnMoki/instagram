5611ff50904d298f94e1b047485fe2b32e049874
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:31:27 +0300

    defined user directory path

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+
+
+from django.db.models.signals import post_save, post_delete
+from django.utils.text import slugify
+from django.urls import reverse
+
+from notifications.models import Notification
+
 
 # Create your models here.
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
