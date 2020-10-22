b83260d5bbbf7be69a85112b13d21377cb2cb507
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:26:32 +0300

    defined index

--- post/views.py
+++ post/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
-# Create your views here.
+from post.models import Stream, Post, Tag, Likes
+from post.forms import NewPostForm
+
+from comment.models import Comment
+from comment.forms import CommentForm
+
+
+from django.contrib.auth.decorators import login_required
+
+from django.urls import reverse
+from authy.models import Profile
+
+
+@login_required
+def index(request):
+	user = request.user
+	posts = Stream.objects.filter(user=user)
+
+
+
+	group_ids = []
+
+	for post in posts:
+		group_ids.append(post.post_id)
+		
+	post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')		
+
+	template = loader.get_template('index.html')
+
+	context = {
+	'post_items': post_items,
+
+	}
+
+	return HttpResponse(template.render(context, request))
