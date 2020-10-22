dc87b26fb8cc641db77691b98864985f42050b54
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:58:36 +0300

    updates

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from post.views import index, NewPost, PostDetails, tags, like, favorite
+from post.views import home, NewPost, PostDetails, tags, like, favorite
 
 
 urlpatterns = [
-   	path('', index, name='index'),
+   	path('', home, name='home'),
    	path('newpost/', NewPost, name='newpost'),
    	path('<uuid:post_id>', PostDetails, name='postdetails'),
    	path('<uuid:post_id>/like', like, name='postlike'),
