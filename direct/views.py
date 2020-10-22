fd9f69d4a3731a4a9939b0a706cc13dd2d107299
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:10:07 +0300

    defined inbox

--- direct/views.py
+++ direct/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template import loader, RequestContext
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User
+from direct.models import Message
+
+
+from django.db.models import Q
+from django.core.paginator import Paginator
+
+@login_required
+def Inbox(request):
+	messages = Message.get_messages(user=request.user)
+	active_direct = None
+	directs = None
+
+	if messages:
+		message = messages[0]
+		active_direct = message['user'].username
+		directs = Message.objects.filter(user=request.user, recipient=message['user'])
+		directs.update(is_read=True)
+		for message in messages:
+			if message['user'].username == active_direct:
+				message['unread'] = 0
+
+	context = {
+		'directs': directs,
+		'messages': messages,
+		'active_direct': active_direct,
+		}
+
+	template = loader.get_template('direct/direct.html')
+
+	return HttpResponse(template.render(context, request))
+
