944d92e01881758806a80cdf26a6d144da90fe10
Ronny <mwithiamoki@gmail.com>
2020-10-22 03:21:31 +0300

    defined count no notifications

--- notifications/views.py
+++ notifications/views.py
@@ -23,3 +23,12 @@ def DeleteNotification(request, noti_id):
 	Notification.objects.filter(id=noti_id, user=user).delete()
 	return redirect('show-notifications')
 
+def CountNotifications(request):
+	count_notifications = 0
+	if request.user.is_authenticated:
+		count_notifications = Notification.objects.filter(user=request.user, is_seen=False).count()
+
+	return {'count_notifications':count_notifications}
+	
+
+
